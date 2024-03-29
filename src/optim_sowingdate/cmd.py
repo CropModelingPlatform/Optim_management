3cd2606072a572fb639d478252b19c554459ff0d
Cyrille MIDINGOYI <34443570+cyrillemidingoyi@users.noreply.github.com>
2024-03-29 15:55:46 +0100

    Update cmd.py

--- src/optim_sowingdate/cmd.py
+++ src/optim_sowingdate/cmd.py
@@ -5,7 +5,7 @@ from pathlib import Path
 import click
 
 os.environ["CMD_MODE"] = "1"
-__version__ = "2.0.11"
+__version__ = "1.0.1"
 
 @click.group()
 @click.version_option(version=__version__, prog_name="optimal_sowingdate")
@@ -17,13 +17,11 @@ def cli():
 @click.argument("prefix")
 def cmd_optimize(modelout, resultpath):
     """Optimize sowing date for given model outputs.
-
     
     modelout: the path of the folder containing the model outputs
     resultpath: the path of the folder where sowing date netcdf files will be saved
     """
     optimize(modelout, resultpath)
-
 cli.add_command(cmd_optimize)
 
 if __name__ == "__main__":
